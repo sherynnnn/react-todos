cfb3d115647a736d9fdc5306602ae098521adee5
sherynnnn <142745064+sherynnnn@users.noreply.github.com>
2024-11-14 15:42:59 +0800

    Update addnew.js

--- src/components/addnew.js
+++ src/components/addnew.js
@@ -1,21 +1,39 @@
-function AddNewForm() {
-    return (
-      <div>
-        <form>
-          <div className="mt-4 d-flex justify-content-between align-items-center">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Add new item..."
-              name="item_name"
-              required
-            />
-            <button className="btn btn-primary btn-sm rounded ms-2">Add</button>
-          </div>
-        </form>
-      </div>
-    );
+import{useState} from 'react';
+
+function AddNewForm(props) {
+
+  const {onNewTaskAdded} = props
+  const [taskName,setTaskText] = useState("");
+
+  return (
+    <div>
+      <form>
+        <div className="mt-4 d-flex justify-content-between align-items-center">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Add new item..."
+            name="taskText"
+            value={taskName}
+            required
+            onChange={(event) => setTaskText(event.target.value)}
+          />
+          <button className="btn btn-primary btn-sm rounded ms-2" onClick={(event) => {event.preventDefault();
+            if(taskName === "") {
+              alert("Please enter the text");
+            }else {
+              onNewTaskAdded(taskName);
+              setTaskText("");
+            }
+          }}>Add</button>
+          
+        </div>
+      </form>
+    </div>
+  );
   }
+   
+  
   
   export default AddNewForm;
   
