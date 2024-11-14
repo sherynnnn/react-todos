eb72305a3b4e5137ead312f17e909a2823faa7b1
sherynnnn <142745064+sherynnnn@users.noreply.github.com>
2024-11-14 15:44:07 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
+import React, {useState} from 'react';
+import {nanoid} from 'nanoid'
 import AddNewForm from "./components/addnew";
 import ItemList from "./components/list";
 
 function App() {
 
-  const list = [
-    {
-      id: 1,
-      text: "Task 1",
-      isCompleted: true
-    },
-    {
-      id: 2,
-      text: "Task 2",
-      isCompleted: false
-    },
-    {
-      id: 3,
-      text: "Task 3",
-      isCompleted: false
-    }
-  ];
+  const [list, setList] = useState ([])
+
 
   return (
     <div className="App">Todo
@@ -32,10 +19,42 @@ function App() {
     >
       <div className="card-body">
         <h3 className="card-title mb-3">My Todo List</h3>
-        <ItemList list={list}/>
+        <ItemList
+          list={list}
+          deleteTask={(id) => {
+            // filter OUT the task with the given id
+            const newList = list.filter((t) => t.id !== id);
+            // update the newList with the setState function
+            setList(newList);
+          }}
+          
+          toggleTask={(id) => {
+            const taskList = list.map((item) => {
+              if (item.id === id) {
+                return {...item, isCompleted: !item.isCompleted};
+              } else{
+              return item;
+              }
+            });
+            setList(taskList);
+          }}
+        />
         
-          <AddNewForm/>
-        </div>
+        <AddNewForm
+          onNewTaskAdded={(taskName) => {
+            // clone the existing state
+            const newList = [...list];
+            // push the new item into the newList
+            newList.push({
+              id: nanoid(), // generate id
+              // id: newList.length + 1,
+              text: taskName,
+            });
+            // update the newList with the setState function
+            setList(newList);
+          }}
+        />
+        </div>      
       </div>
      </div>
   );
