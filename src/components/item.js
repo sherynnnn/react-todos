2103fd455f134e7599e0f32795180aa9b2974bae
sherynnnn <142745064+sherynnnn@users.noreply.github.com>
2024-11-14 15:43:20 +0800

    Update item.js

--- src/components/item.js
+++ src/components/item.js
@@ -1,15 +1,24 @@
-function Item(props) {
-    const {isCompleted = true, text = "No task" }= props;
+import React from 'react';
+
+
+function Item({task, toggleTask,deleteTask}) {
+    const {id , text, isCompleted }= task;
     return (
       <li className="list-group-item d-flex justify-content-between align-items-center">
         <div>
-        <button className={`btn btn-sm ${isCompleted ? 'btn-success' : 'btn-light'}`} >
+        <button className={`btn btn-sm ${isCompleted ? 'btn-success' : 'btn-light'}`} onClick={() => toggleTask(id)}>
         <i className={`bi ${isCompleted ? 'bi-check-square' : 'bi-square'}`}></i>
         </button>
         <span className={`ms-2 ${isCompleted ? 'text-decoration-line-through' : ''}`}>
         {text}</span>
         </div>
-        <button class="btn btn-sm btn-danger">
+        <button className="btn btn-sm btn-danger" onClick={() => {const confirm =window.confirm("Are you sure want to delete this task");
+          if (confirm) {
+            deleteTask(id)
+          }
+          }
+        }
+         >
                 <i class="bi bi-trash"></i>
           </button>
       </li>
